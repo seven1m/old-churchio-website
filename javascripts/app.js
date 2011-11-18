beaf0a16a7e287df5af24d250e5466d59dafdb04
Tim Morgan <tim@timmorgan.org>
2011-11-18 17:24:03 -0600

    Improve install/upgrade interaction.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,9 +1,11 @@
 function showInstallBlock(platform) {
   platform = typeof(platform) == 'string' ? platform.replace('#', '') : platform.target.id.substring(5);
-  $('.windows, .mac, .linux').hide();
-  $('.' + platform).show();
-  $('.tabs li').removeClass('active');
-  $('#show-' + platform).parent().addClass('active');
-  location.hash = platform;
+  if(platform == 'windows' || platform == 'mac' || platform == 'linux') {
+    $('.windows, .mac, .linux').hide();
+    $('.' + platform).show();
+    $('.tabs li').removeClass('active');
+    $('#show-' + platform).parent().addClass('active');
+    location.hash = platform;
+  }
   return false;
 }
